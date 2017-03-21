a0b8d4eaa742d4aaadd1c33bf0601c64740853e7
Kundan <kundansable@gmail.com>
2017-03-22 01:06:23 +0530

    setup.py updated for setuptools

--- setup.py
+++ setup.py
@@ -7,22 +7,25 @@ try:
 except ImportError:
     pass
 
-from setuptools import setup, find_packages
+try:
+    from setuptools import setup, find_packages
+except AttributeError:
+    from setuptools import setup, find_packages
 
 setup(
-    name = "DjangoSEO",
-    version = '1.0',
-    packages = find_packages(exclude=["docs*", "regressiontests*"]),
-    namespace_packages = ['rollyourown'],
-    requires = ['django (>=1.1)'],
-    author = "Will Hardy",
-    author_email = "djangoseo@willhardy.com.au",
-    description = "A framework for managing SEO metadata in Django.",
-    long_description = open('README').read(),
-    license = "LICENSE",
-    keywords = "seo, django, framework",
-    url = "https://github.com/willhardy/django-seo",
-    include_package_data = True,
+    name="DjangoSEO",
+    version='1.0',
+    packages=find_packages(exclude=["docs*", "regressiontests*"]),
+    namespace_packages=['rollyourown'],
+    requires=['django (>=1.1)'],
+    author="Will Hardy",
+    author_email="djangoseo@willhardy.com.au",
+    description="A framework for managing SEO metadata in Django.",
+    long_description=open('README').read(),
+    license="LICENSE",
+    keywords="seo, django, framework",
+    url="https://github.com/willhardy/django-seo",
+    include_package_data=True,
     classifiers=[
         "Development Status :: 4 - Beta",
         "Environment :: Web Environment",
@@ -35,4 +38,3 @@ setup(
         "Topic :: Software Development"
     ],
 )
-
